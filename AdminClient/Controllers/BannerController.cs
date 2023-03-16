7101a754fdfc0752bec266eb6601ba55bef960a3
Harekrushna <hmaharana1998@gmail.com>
2023-03-16 12:24:43 +0530

    Banner

---
+++ AdminClient/Controllers/BannerController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminClient.Controllers
+{
+    public class BannerController : Controller
+    {
+        public IActionResult BannerCreate()
+        {
+            return View();
+        }
+        public IActionResult BannerList()
+        {
+            return View();
+        }
+        public IActionResult BannerEdit(int bannerId)
+        {
+            ViewBag.bannerId = bannerId;
+            return View();
+        }
+    }
+}
